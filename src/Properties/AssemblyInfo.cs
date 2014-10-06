7778b6aec37d725ec189f872b6379a9528292c1d
Chris van de Steeg <chris.vande.steeg@efocus.nl>
2014-10-06 22:53:20 +0200

    Nuget ready

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Efocus.Sitecore.ConditionalConfig")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use the sitecore patch configs only on machines that it applies to")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("eFocus")]
 [assembly: AssemblyProduct("Efocus.Sitecore.ConditionalConfig")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
